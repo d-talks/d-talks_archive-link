6b8d44403031f9f976a3c04b52bf780d5fa2d72a
ohsung cho <fifthsage@gmail.com>
2019-06-15 17:18:09 +0900

    host

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,13 @@ module.exports = {
       name: 'd-talks_link-archive',
       script: './server/index.js',
       env: {
+        HOST: '0.0.0.0',
         PORT: 3000,
         NODE_ENV: 'development'
       },
       env_production: {
         PORT: 3000,
+        HOST: '0.0.0.0',
         NODE_ENV: 'production'
       }
     }
--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'D Talks Link Archive',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: 'D Talks Toy Project'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
