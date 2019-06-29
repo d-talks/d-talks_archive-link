82bc2aa2d060dfd35b6b61839ef50997904d0ff2
Lee WonHo <lwh8989@naver.com>
2019-06-29 17:08:47 +0900

    [update] 지원님 작업 업데이트

--- components/card/ji-won/index.vue
+++ components/card/ji-won/index.vue
@@ -1,6 +1,21 @@
 <template>
-  <div class="">
-    지원 카드
+  <div class="root">
+    <div class="module-card card-size-454 jw">
+        <div class="card-visual">
+            <img src="https://dummyimage.com/454x630/00a2ff/fff" alt="card visual image">
+        </div>
+        <div class="card-contents">
+            <div class="text-title">
+                title text
+            </div>
+            <div class="text-sub">
+                sub text sub text sub text sub text sub text sub text
+            </div>
+        </div>
+        <div class="card-footer">
+            <i class="heart outline icon"></i>
+        </div>
+    </div>
   </div>
 </template>
 
